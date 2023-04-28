190d9891a36d94908a8d7730cfa6076a446ab804
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-04-27 20:38:25 -0400

    Make Navbar responsive, add Footer, add some CSS and clean up code

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer>
-			<p>© Lukas Pena | All Rights Reserved</p>
+			<p>© GoodEats | All Rights Reserved</p>
 		</footer>
 	);
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,26 @@
+import { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
-	return (
-		<nav>
-			<h1><Link to="/" id="logo">GoodEats</Link></h1>
-			<ul>
-				<li><Link to="/">Home</Link></li>
-				<li><Link to="/menu">Menu</Link></li>
-				<li><Link to="/order">Order</Link></li>
-				<li><Link to="/about">About</Link></li>
-			</ul>
-		</nav>
-	);
+export default class Navbar extends Component {
+	state = { clicked: false };
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	}
+	render() {
+		return (
+			<nav>
+				<h1><Link to="/" id="logo">GoodEats</Link></h1>
+				<ul id="navbar" className={this.state.clicked ? "#navbar clicked" : "#navbar"}>
+					<li><Link to="/">Home</Link></li>
+					<li><Link to="/menu">Menu</Link></li>
+					<li><Link to="/order">Order</Link></li>
+					<li><Link to="/about">About</Link></li>
+				</ul>
+
+				<div className="mobile" onClick={this.handleClick}>
+					<img src="./icons8-menu-24.png" alt="Other Links" width={25} />
+				</div>
+			</nav>
+		);
+	}
 }
