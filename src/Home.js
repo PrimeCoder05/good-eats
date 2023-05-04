90e010d4574691d48b35f758a61cb2753ca859fe
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-05-04 18:48:54 -0400

    Finish adding styles to home page and add divs for later

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,24 @@ export default function Home() {
 	return (
 		<>
 			<div className="wrapper">
-				<div className="main">
+				<div className="intro" style={{ backgroundImage: `url(./main-background.jpg)` }}>
 					<h2>GoodEats</h2>
 					<p><em>"Fast food done right"</em></p>
 				</div>
-				<h3>Be sure to check out our menu</h3>
-				<Link to="/menu">Go to menu</Link>
-				<h3>Order you food for delivery or pick up</h3>
-				<Link to="/order">Order online</Link>
-				<h3>Read about us and what we are about</h3>
-				<Link to="/about">About us</Link>
+				<main>
+					<div>
+						<h3>Be sure to check out our menu:</h3>
+						<Link to="/menu">Go to menu {'>'}</Link>
+					</div>
+					<div>
+						<h3>Order you food for delivery or pick up:</h3>
+						<Link to="/order">Order online {'>'}</Link>
+					</div>
+					<div>
+						<h3>Read about us and what we are about:</h3>
+						<Link to="/about">About us {'>'}</Link>
+					</div>
+				</main>
 			</div>
 		</>
 	);
